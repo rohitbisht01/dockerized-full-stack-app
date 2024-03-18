7630b9b73547ff4396d91216ac803e8d0af6ee8c
rohitbisht01 <rb9519208@gmail.com>
2024-03-18 23:04:43 +0530

    added .koyeb

--- server-app/index.js
+++ server-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 const app = express();
 app.use(cors());
